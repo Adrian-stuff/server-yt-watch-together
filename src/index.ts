5bed7652753d59e28030549bc7e021cf064a6c76
Adrian-stuff <68731858+Adrian-stuff@users.noreply.github.com>
2022-08-29 18:35:06 +0800

    change comeback song

--- src/index.ts
+++ src/index.ts
@@ -106,9 +106,9 @@ io.on("connection", (socket: Socket) => {
       ) {
         connectedUsers[socket.id] = data;
         socket.join(data.room);
-        // if videodata dont have value then set vidid to scientist(vPwaXytZcgI)
+        // if videodata dont have value then set vidid to talk that talk (k6jqx9kZgPM)
         if (!isDataHas(data.room)) {
-          setVideoData("vPwaXytZcgI", data.room);
+          setVideoData("k6jqx9kZgPM", data.room);
           console.log("no videoid video id set.");
         }
         const videoData: Map<any, any> = getVideoData(data.room);
